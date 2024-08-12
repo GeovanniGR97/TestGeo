056c5349a6d9d5332a1b3f311550064cd14097fb
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 13:59:06 -0600

    tercer cambio

---
+++ edad_if.py
@@ -0,0 +1,7 @@
+edad = 18
+edadPersona = int(input("Ingresa tu edad : "))
+if edadPersona >= edad:
+    print(f'La persona de edad {edadPersona} es mayor de edad')
+else:
+    print(f'La persona de edad {edadPersona} es menor de edad')
+    
