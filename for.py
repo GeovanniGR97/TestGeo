6d8a4983e2cd8a0d2bbcbf682ffcb92371d89fc9
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:25:18 -0600

    Ciclo for

---
+++ for.py
@@ -0,0 +1,33 @@
+"""
+cadena = 'Geovanni'
+for letra in cadena:
+    print(letra)
+else:
+    print('Fin del ciclo')
+
+
+testFor = 'Jose Geo'
+for name in testFor:
+    print(name)
+else:
+    print('Fin del cucle')
+
+nombre = 'jose'
+
+ejmBucle = [1,2,4,5,6,7]
+for datosBucle in ejmBucle:
+    print(datosBucle)
+else:
+    print("Fin del bucle ")
+
+"""
+i  = [1,2,3,4,5,6,7,8,9]
+for i in range(6):
+    print(i)
+
+
+
+
+
+
+
