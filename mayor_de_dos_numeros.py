337998d13cb0f345b7c182c3fd12b1fd09e1022e
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:36:51 -0600

    Comparación de dos numeros

---
+++ mayor_de_dos_numeros.py
@@ -0,0 +1,6 @@
+numeroUno = int(input("Escribe el numero uno: "))
+numeroDos = int(input("Escribe el numero dos: "))
+if numeroUno > numeroDos:
+    print("El numero 1 es mayor")
+else:
+    print("El numero 2 es menor") 
