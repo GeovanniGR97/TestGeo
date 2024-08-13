df8da81f07545c7a367ac71629206a4c8704feb1
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:26:51 -0600

    Funcion input

---
+++ input.py
@@ -0,0 +1,4 @@
+# Es procesada la entrada y es asignada a la variable de  resultado 
+resultado = input("Escribe un hola para poder testear el codigo : ")
+print("Resultado esperado: " + resultado)
+print("Ejecucion exitosa")
