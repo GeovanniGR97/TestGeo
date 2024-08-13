0113c10e68f35280bdd8abeae267f7c0a5ae63ff
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:42:19 -0600

    Operadores logicos

---
+++ operadores_logicos.py
@@ -0,0 +1,17 @@
+#Operador AND 
+#True And True regresa True
+#True And False regresa False
+a = True
+b = False
+#resultado = a and b 
+resultado = a or b 
+#resultado = a and b 
+
+#Operador OR
+#True or True regresa True 
+#True or False regresa True 
+#False or True regresa True 
+print(resultado)
+
+#Con el operado "not" se invierten los valores ejemplo: Si tenemos un operador com true lo manda a falso a = True regresara como False
+
