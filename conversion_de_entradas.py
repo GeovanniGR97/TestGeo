c9d8d499a6c244328affb2d8783299c43aeb490e
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:10:25 -0600

    Conversión de entradas en python

---
+++ conversion_de_entradas.py
@@ -0,0 +1,5 @@
+datosEmpresa = input("¿cual es el  nombre de la empresa ")
+anioEmpresa = int(input("¿Año que entraste a trabajar "))
+
+print ("La empresa donde trabajo se llama ", datosEmpresa, " entre a trabajar en el año ",  str(anioEmpresa))
+
