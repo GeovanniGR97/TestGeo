8df9261b1755485584e37fc1b3d88196e583eb5f
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:07:35 -0600

    concatenacion en python

---
+++ concatenacion.py
@@ -0,0 +1,3 @@
+bandaFavorita = "Guns and roses" + " y "  + "METALLICA"
+cancionFavorita = "DONT CRY men"
+print("Mi banda favorita es: " + bandaFavorita + " " + "y mi cancion favorita es " , cancionFavorita)
