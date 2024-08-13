3a518fd46c13925348ab26123a883943f2bce9dc
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:15:27 -0600

    Datos booleanos

---
+++ datos_boolean.py
@@ -0,0 +1,8 @@
+valUno = 20
+valDos = 20
+valDivision = valUno / valDos
+valUser = int(input("La respuesta es: "))
+if valDivision == valUser:
+    print("El resultado es correcto")
+else:
+    print("El resultado es incorrecto")
