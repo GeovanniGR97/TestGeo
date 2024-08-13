35c90dde4652478de530ed41e705c8fc56ae3b1b
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:31:34 -0600

    If else simplificado

---
+++ if_else_simplificado.py
@@ -0,0 +1,18 @@
+#EJEMPLO DE UN OPERADOR TERNEARIO 
+#condicion = True
+#if condicion: 
+ #   print("Es verdadero")
+#else:
+ #   print("Es falso")
+
+
+#print("La condición es verdadera ") if condicion else print('Condicion Falsa')
+'''
+x = 4
+resultado = "Es par" if x % 2 == 0 else "es impar"
+print(resultado)
+'''
+valorNumero = int(input("Ingresa el numero: "))
+valorNum = "Es par" if valorNumero % 2 == 0 else "es impar"
+print(f"Tu número es {valorNumero} y es {valorNum}")
+
