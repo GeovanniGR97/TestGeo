b3a2d3f8c35f8f50a24a23f260f3bd3660a20dde
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-12 21:44:50 -0600

    Ejercicio area de un rectangulo

---
+++ area_rectangulo.py
@@ -0,0 +1,27 @@
+'''operadorA = 3
+operadorB = 5
+suma = operadorA + operadorA
+print(f"El resultado de la suma es:  {suma}")
+'''
+#Calcular el area y el perimetro de un rectangulo
+
+'''
+alto = 10
+ancho = 10
+triangulo = alto * ancho 
+print(f"El area del triangulo es: {triangulo} " )
+'''
+
+
+
+
+
+altoArea = int(input("Escribe la altura del rectangulo:  "))
+anchoArea = int(input("Escribe el ancho del rectangulo:  "))
+areaRectangulo = altoArea * anchoArea
+perimetroRectangulo = (altoArea + anchoArea) * 2
+print(f"El resultado del area del rectangulo es: {areaRectangulo}")
+print(f"El resultado del perimetro del rectangulo es: {perimetroRectangulo}")
+
+
+
