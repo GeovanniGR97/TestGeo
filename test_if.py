b02d95f7fa107e042ccf821d9b8e42044aeef668
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-16 11:58:02 -0600

    Ejemplo simplificado de if, elif, else

---
+++ test_if.py
@@ -0,0 +1,8 @@
+edad = int(input("Ingresa tu edad: "))
+
+if edad > 18:
+    print("Eres mayor de edad")
+elif edad == 18:
+    print("Tienes la edad correcta")
+else:
+    print("Eres menor de edad")
