54c7a274f403acc1a36d53743c87d801e02519ea
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-20 12:25:04 -0600

    Cambios  de ortografía en la función

--- funcion.py
+++ funcion.py
@@ -2,11 +2,11 @@
 #Su principal ventaja es que nos ayudan a evitar codigo repetido
 
 
-sueldo = int(input("ingresa tu sueldo :"))
+sueldo = int(input("Ingresa tu sueldo :"))
 def sueldoMexiano(sueldo):
     if sueldo > 1700:
         print(f"Tu sueldo es: {sueldo} ganas mas del sueldo mínimo")
-    elif sueldo == 1700:
+    elif sueldo == 1750:
         print(f"Tu sueldo es: {sueldo} ganas el sueldo mínimo")
     else:
         print(f"Tu sueldo es: {sueldo} no ganas el sueldo minimo")
