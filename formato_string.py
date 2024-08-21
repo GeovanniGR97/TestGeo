15b744407f6c272e593f41e3c189650d122d849c
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-21 12:58:56 -0600

    Nuevos formatos string

---
+++ formato_string.py
@@ -0,0 +1,19 @@
+dolar = 19
+#tipo de formato legal, como .2f que significa número de coma fija con 2 decimales:
+print(f"El precio del dolar esta en {dolar:.2f}")
+
+dolar_two = 19
+print(f"El precio del dolar multiplicado por 20 es: {dolar_two * 20}")
+
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+
+x = 2006
+y = 2024
+z = x - y
+print(z)
+
+
+
