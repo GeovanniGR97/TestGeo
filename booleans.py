62797533abfd286d2933babc48bf2f02b0ac41b0
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-21 20:19:26 -0600

    Primeros ejemplos en tipos de datos booleanos

---
+++ booleans.py
@@ -0,0 +1,20 @@
+a = 9 > 5
+b = 10 == 5
+c = 5 < 10
+print(a, b , c )
+
+x = 10
+y = 20
+if x > y :
+    print(f"{x} Es un número mayor")
+else:
+    print(f"{y} Es un número menor")
+
+x = 30
+y = 20
+if x > y :
+    print("x Es un número mayor y")
+else:
+    print("x Es un número menor y")
+
+    
