8ebd3490197859e8de67f032940339edca20c2c6
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-22 22:48:33 -0600

    Nuevo ejemplo elif

---
+++ ejm.py
@@ -0,0 +1,14 @@
+precioUno = int(input("Ingresa el precio : "))
+precioDos = int(input("Ingresa el segundo precio :"))
+if precioUno > precioDos:
+    print(f"El precio uno: {precioUno} es mayor que el precio dos {precioDos}")
+elif precioUno == precioDos:
+    print(f"El precio uno: {precioUno} es igual al precio dos: 12 {precioDos}")
+else:
+    print(f"El precio dos: {precioDos} es mayor al precio uno {precioUno}")
+
+
+
+
+
+
