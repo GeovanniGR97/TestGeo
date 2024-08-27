88a73e9af8ea5c4ba91e49790a1c00f19f61c53a
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-26 23:19:39 -0600

    Ejemlos de if

--- ejm.py
+++ ejm.py
@@ -1,3 +1,4 @@
+'''
 precioUno = int(input("Ingresa el precio : "))
 precioDos = int(input("Ingresa el segundo precio :"))
 if precioUno > precioDos:
@@ -6,6 +7,103 @@ elif precioUno == precioDos:
     print(f"El precio uno: {precioUno} es igual al precio dos: 12 {precioDos}")
 else:
     print(f"El precio dos: {precioDos} es mayor al precio uno {precioUno}")
+
+    datoNum = int(input("Ingresa el primer número: "))
+dato_num_dos = int(input("Ingresa el segundo número: "))
+if datoNum > dato_num_dos:
+    print(f"El dato uno: {datoNum} es mayor al dato dos: {dato_num_dos}")
+elif datoNum == dato_num_dos:
+    print(f"numero {datoNum} es igual a {dato_num_dos}")
+else:
+    print(f"El dato uno: {dato_num_dos} es menor al dato dos: {datoNum}")
+
+    '''
+def saludo():
+    print("holaa")
+saludo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
