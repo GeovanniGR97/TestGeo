f03f1a782359cb615dac9042042f238dabc4c856
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-28 22:37:18 -0600

    Tuplas con If

---
+++ tuplas_if.py
@@ -0,0 +1,5 @@
+curso = input("Ingresa el curso que deseas tomar : ")
+if curso in ("Matematicas", "Ciencias computacionales", "Programacion"):
+    print (f"Curso {curso} selecionado 😎")
+else:
+    print("El curso no existe")
