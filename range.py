d6e5cffa3397024d84f4847d53967bf9457cad9c
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-09-06 21:07:45 -0600

    Range usando el ciclo FOR

---
+++ range.py
@@ -0,0 +1,24 @@
+#Crea una lista inmutable de números enteros en sucesión aritmetica 
+# range():
+#numeros = 5
+#print(numeros)
+
+
+#Este rango se concentra especificamente de 2 a 7  ya que el numero limite es 8 en donde 8 se cierra el ciclo 
+'''
+for i in range(2, 8):
+    print(i)
+
+#Contar hacia atras 
+for i in range(5, 0, -1):
+    print(i)
+ '''
+#Repetir una acción un número determinado de veces
+'''
+for i in range(3):
+    print("Holaa")
+'''
+#Generación de indices para una lista.... 
+lista = ["Manzana", "Fresa", "Kiwi"]
+for i in range(len(lista)):
+    print(f"Indice {i}: {lista[i]}")
