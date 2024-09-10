4278dc3ec4f5adcf8583b6160843ba91ba64452d
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-09-09 22:20:37 -0600

    Estructura While

---
+++ while_one.py
@@ -0,0 +1,24 @@
+'''
+Estructura repetitiva que nos permite realizar multiples iteraciones
+basandonos en el resultado de una expresión lógica que puede tener 
+como resultado un valor True and False 
+'''
+'''
+indice = 1
+while indice < 11:
+    print(f"Valor actual: {indice}")
+    indice = indice + 1
+print("Termina el ciclo while...")
+'''
+
+inicio = 2 
+while inicio <= 100:
+    print(f"Numero par: {inicio}")
+    inicio += 2
+print("Termino el ciclo.........")
+
+
+
+
+
+
