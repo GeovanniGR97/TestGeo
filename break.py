293446700d8a913905b2c36a10b9dd7a622c8135
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-09-09 22:31:11 -0600

    Break

---
+++ break.py
@@ -0,0 +1,10 @@
+'''
+Break: Permite salir de un bucle cuando se cumple una condición
+Break: Descanso o interrupción en este punto..
+'''
+for numero in range(1,6):
+    if numero == 3:
+        break
+    print(f"El número es: {numero}")
+print("Bucle terminado........")
+
