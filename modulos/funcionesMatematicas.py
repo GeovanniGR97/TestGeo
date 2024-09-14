89040befff37fc8b580d4c597ffd83d685a227e9
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-09-13 21:18:06 -0600

    Modulos funciones matematica

---
+++ modulos/funcionesMatematicas.py
@@ -0,0 +1,10 @@
+def sumar(n1, n2):
+    return n1 + n2, 
+
+
+def multiplicar(n1, n2):
+    return n1 * n2
+
+def division(n1, n2):
+    return n1 / n2
+
---
+++ modulos/modulos.py
@@ -0,0 +1,20 @@
+'''
+Archivo con extesión .py o pyc (Python copilado)
+Posee su propio espacio de nombres y que puede contener: 
+
+Variables, Funciones, clases o incluso otros modulos 
+Para que sirve:
+Organiza y reutiliza mejor el codigo 
+Modularización y reutilización...
+
+from = Desde donde quiero importar el archivo
+
+ Al integrar la un * se mandan a llamar todas las funciones que tenemos en el otro archivo 
+'''
+from funcionesMatematicas import *
+#import funcionesMatematicas
+print(sumar(5,5))
+print(multiplicar(5,5))
+print(division(10,2))
+
+
