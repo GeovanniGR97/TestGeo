2e8b0159b550b9fd0d7ce43279eee653c1f2c540
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-09-17 21:14:49 -0600

    Ejercicios While

--- while.py
+++ while.py
@@ -1,6 +1,21 @@
-contador = 1
+
+'''contador = 1
 while contador <= 5:
     print(contador)
     contador +=1
 else:
     print('Bucle completado')
+
+contador_Numeros = 1
+while contador_Numeros <= 10:
+    print(contador_Numeros)
+    contador_Numeros +=1
+else: 
+    print('Bucle finalizado...')
+'''
+
+#while = mientras
+questionMoon = int(input("¿Cuantas lunas tiene Jupiter?: "))
+while questionMoon != 95:
+    questionMoon = int(input("Respuesta equivocada, responde de nuevo : "))
+print("Excelente respuesta.")
