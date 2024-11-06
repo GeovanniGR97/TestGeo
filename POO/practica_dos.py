38b3015964e9fc4222c2b29635d085e1dff95eb2
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-11-06 14:02:45 -0600

    Segunda practica de encapsulamiento, get y set

---
+++ POO/practica_dos.py
@@ -0,0 +1,69 @@
+class Pokemon:
+    __tipo = 'Electrico'  # Atributo de clase
+    
+    def __init__(self, nombre, color, nivel, voltaje_max, amperaje_max):
+        self.nombre = nombre
+        self.color = color
+        self.__nivel = nivel
+        self.__voltaje_max = voltaje_max
+        self.__amperaje_max = amperaje_max
+
+    # Propiedad para acceder a `__nivel`
+    @property
+    def nivel(self):
+        return self.__nivel
+
+    # Setter para modificar `__nivel` con validación
+    @nivel.setter
+    def nivel(self, nivel):
+        if nivel >= 0 and nivel <=100:
+            self.__nivel = nivel
+        else:
+            print("El nivel no puede ser negativo.")
+
+    @property
+    def voltaje_max(self):
+        return self.voltaje_max
+    
+    @voltaje_max.setter
+    def voltaje_max(self, voltaje_maximo):
+        if voltaje_maximo > 0 and voltaje_maximo <=100:
+            self.__voltaje_max = voltaje_maximo
+        else:
+            print('El voltaje no puede ser mayor a 100')
+
+    @property
+    def amperaje_max(self):
+        return self.amperaje_max
+    
+    @amperaje_max.setter
+    def amperaje_max(self, amperaje_max):
+        if amperaje_max > 0 and amperaje_max <=100:
+            self.amperaje_max = amperaje_max
+        else: print('El amperaje no puede ser mayor a 100')
+
+
+    # Método para obtener el nivel (opcional)
+    #def get_nivel(self):
+    #   return self.__nivel
+
+    # Método para realizar el ataque
+    def atacar(self):
+        print(f'{self.nombre} tiene un nivel de ataque de {self.__nivel / 4}')
+
+# Crear una instancia de `Pokemon`
+volvasor = Pokemon('Volvasor', 'Verde', 215130, 5412351, 2630394)
+
+# Mostrar información inicial del Pokémon
+print(f'El pokemon {volvasor.nombre} tiene un nivel {volvasor.nivel}')
+
+# Cambiar el nivel a un valor positivo (válido)
+volvasor.nivel = 300000
+print(f'Nuevo nivel de {volvasor.nombre}: {volvasor.nivel}')
+
+# Intentar establecer un nivel negativo (inválido)
+volvasor.nivel = -100
+print(f'Nivel actual de {volvasor.nombre}: {volvasor.nivel}')
+
+# Ejecutar el ataque para ver el nivel de ataque
+volvasor.atacar()
